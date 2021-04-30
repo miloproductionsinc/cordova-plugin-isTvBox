ee1a0a18c0998521f2259f25a1d2f797af602459
Sufian <sufian@muslimkids.tv>
2021-04-30 12:49:36 +0500

    Refactored isTVBox.java.

--- src/android/IsTvBox.java
+++ src/android/IsTvBox.java
@@ -1,5 +1,4 @@
 package com.shahid.cordova.plugin;
-import android.widget.Toast;
 // Cordova-required packages
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -8,49 +7,41 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.app.UiModeManager;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.util.Log;
-import android.content.Context;
+
 public class IsTvBox extends CordovaPlugin {
   public static final String TAG = "DeviceTypeRuntimeCheck";
-  public static final String UI_MODE_SERVICE = "uimode";
-  public Context context;
+  
   @Override
   public boolean execute(String action, JSONArray args,
     final CallbackContext callbackContext) throws JSONException {
+      // TODO needed for multiple functionalities. Later on, verify if we need to keep it.
       // Verify that the user sent a 'checkTvBox' action
       if (!action.equals("checkTvBox")) {
         callbackContext.error("\"" + action + "\" is not a recognized action.");
         return false;
       }
-     
-    
-      // will check that if app is running on android Tv or Android mobile
-      JSONObject r = new JSONObject();
-            r.put("isTvBox", this.isTv());
-            r.put("isTouchScreen", this.isTouch());
+
+      JSONObject result = new JSONObject()
+            .put("isTv", isTv())
+            .put("hasTouchScreen", hasTouchScreen());
           
-      callbackContext.success(r);
+      callbackContext.success(result);
       return true;
-     
-     
   }
 
-  public boolean isTv(){
+  private boolean isTv(){
     UiModeManager uiModeManager = (UiModeManager) cordova.getActivity().getApplicationContext().getSystemService(Context.UI_MODE_SERVICE);
-    if (uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION) {
-        Log.d(TAG, "TV Device");
-        
-       return true;
-       
-    } else {
-        Log.d(TAG, "Non-TV Device");
-     
-      return false;
-       
-    }
+    boolean isTv = uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION;
+
+    Log.d(TAG, isTv ? "TV Device" : "Non-TV Device");
+
+    return isTv;
   }
-  public boolean isTouch(){
+
+  private boolean hasTouchScreen(){
     return cordova.getActivity().getApplicationContext().getPackageManager().hasSystemFeature("android.hardware.touchscreen");
   }
 }
